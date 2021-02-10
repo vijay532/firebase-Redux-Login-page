84249502cb15b99c7c649c948a46e864b4c52235
vijay <sahaniv66@gmail.com>
2021-02-10 16:40:23 +0530

    firebase login page with redux

---
+++ src/components/Login.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { loginUser } from "../actions";
+import { withStyles } from "@material-ui/styles";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import { auth } from '../firebase/firebase'
+const styles = () => ({
+  "@global": {
+    body: {
+      backgroundColor: "#fff"
+    }
+  },
+  paper: {
+    marginTop: 100,
+    display: "flex",
+    padding: 20,
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    backgroundColor: "#f50057"
+  },
+  form: {
+    marginTop: 1
+  },
+  errorText: {
+    color: "#f50057",
+    marginBottom: 5,
+    textAlign: "center"
+  }
+});
+
+class Login extends Component {
+  state = { email: "", password: "" };
+
+  handleEmailChange = ({ target }) => {
+    this.setState({ email: target.value });
+  };
+
+  handlePasswordChange = ({ target }) => {
+    this.setState({ password: target.value });
+  };
+
+  handleSubmit = () => {
+    const { dispatch } = this.props;
+    const { email, password } = this.state;
+
+    dispatch(loginUser(email, password));
+  };
+
+  handleSignUp=event=>{
+    event.preventDefault();  //for avoid loss of data after refreshing 
+    console.log("this is registration page")
+    auth.createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then((userCredential) => {
+        // Signed in 
+        console.log(userCredential.email);
+        // ...
+        // history.push({
+        //     pathname:'/register',
+        //     state:{detail:email}
+        // })
+    })
+    .catch((error) => {
+        alert(error.message)
+        // ..
+    });
+  }
+
+  render() {
+    const { classes, loginError, isAuthenticated } = this.props;
+    if (isAuthenticated) {
+      return <Redirect to="/" />;
+    } else {
+      return (
+        <Container component="main" maxWidth="xs">
+          <Paper className={classes.paper}>
+            
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              onChange={this.handleEmailChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={this.handlePasswordChange}
+            />
+            {loginError && (
+              <Typography component="p" className={classes.errorText}>
+                Incorrect email or password.
+              </Typography>
+            )}
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={this.handleSubmit}
+            >
+              Sign In
+            </Button>
+            <button onClick={(event) => this.handleSignUp(event)} className="login__registerButton">Create your Account</button>
+          </Paper>
+        </Container>
+      );
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isLoggingIn: state.auth.isLoggingIn,
+    loginError: state.auth.loginError,
+    isAuthenticated: state.auth.isAuthenticated
+  };
+}
+
+export default withStyles(styles)(connect(mapStateToProps)(Login));
---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  //Your config values
+  apiKey: "AIzaSyB-MhZdPYNY6wUJQduIxUIwcjY6ZKJSS7w",
+	authDomain: "testpage-e4c04.firebaseapp.com",
+	projectId: "testpage-e4c04",
+	storageBucket: "testpage-e4c04.appspot.com",
+	messagingSenderId: "897464993015",
+	appId: "1:897464993015:web:2b773f1eea828539dbfff2",
+	measurementId: "G-TQDNP67Z5T"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
